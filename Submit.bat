2b4ac86da5a4fe516b8d06460725b86ed259f6ca
cacho <394369786@qq.com>
2017-11-18 18:28:55 +0800

    update submit.bat

--- Submit.bat
+++ Submit.bat
@@ -1,15 +1,15 @@
 @echo off
 
 f:
-cd F:\SoftwareTest\zeng\02 InterfaceTest
+cd F:\SoftwareTest\Github\01 IntroduceDocument
 
 git init
-git remote add gitee https://gitee.com/Cacho/InterfaceTest.git
-git remote add github https://github.com/Cachozeng/InterfaceTest.git
+git remote add gitee https://gitee.com/Cacho/IntroduceDocument.git
+git remote add github https://github.com/Cachozeng/IntroduceDocument.git
 
 git pull gitee master
 git add .
-git commit -m "add InterfaceTest folder"
+git commit -m "add IntroduceDocument folder"
 git push gitee master
 git push github master
 
